a791cfec4f95c1724764e6494d741386ca8f1ab7
Andrew Dodd <atd7@cornell.edu>
2012-09-12 21:57:51 -0400

    wifi: Allow Settings to retrieve country code
    This makes getCountryCode behave like getFrequencyBand
    
    This allows Settings to get/set country code like
    it can for frequency band.
    
    Change-Id: Id9f00d3a678642c6c142fd1511af820d729507a7
    
    Conflicts:
    	services/java/com/android/server/WifiService.java

--- wifi/java/android/net/wifi/WifiStateMachine.java
+++ wifi/java/android/net/wifi/WifiStateMachine.java
@@ -212,6 +212,9 @@ public class WifiStateMachine extends StateMachine {
     /* Tracks current frequency mode */
     private AtomicInteger mFrequencyBand = new AtomicInteger(WifiManager.WIFI_FREQUENCY_BAND_AUTO);
 
+    /* Tracks current country code */
+    private String mCountryCode = "GB";
+
     /* Tracks if we are filtering Multicast v4 packets. Default is to filter. */
     private AtomicBoolean mFilteringMulticastV4Packets = new AtomicBoolean(true);
 
@@ -1056,6 +1059,13 @@ public class WifiStateMachine extends StateMachine {
                     countryCode);
         }
         sendMessage(CMD_SET_COUNTRY_CODE, countryCode);
+    }
+
+    /**
+     * Returns the operational country code
+     */
+    public String getCountryCode() {
+        return mCountryCode;
     }
 
     /**
@@ -2495,7 +2505,9 @@ public class WifiStateMachine extends StateMachine {
                 case CMD_SET_COUNTRY_CODE:
                     String country = (String) message.obj;
                     if (DBG) log("set country code " + country);
-                    if (!mWifiNative.setCountryCode(country.toUpperCase())) {
+                    if (mWifiNative.setCountryCode(country.toUpperCase())) {
+                        mCountryCode = country;
+                    } else {
                         loge("Failed to set country code " + country);
                     }
                     break;
