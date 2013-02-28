518b6d3b7962f9902586ecb1acf4942e7121e82d
Bruno Randolf <br1@einfach.org>
2013-02-28 23:19:28 +0000

    Wifi: Add IBSS supported method
    Add method isIbssSupported() to WifiManager and related classes to query wether
    the driver supports IBSS mode. The query is answered by wpa_supplicants new
    command "get_capability modes".
    
    It will only return a correct result after WIFI_STATE_ENABLED has been
    reached.
    
    Change-Id: I93aa2fdff94cd897c91ec3beb7e58940d1de7b72
    
    Conflicts:
    	wifi/java/android/net/wifi/WifiNative.java
    
    Signed-off-by: iceandfire <arhamjamal@gmail.com>
    
    Conflicts:
    	wifi/java/android/net/wifi/WifiNative.java
    	wifi/java/android/net/wifi/WifiStateMachine.java

--- wifi/java/android/net/wifi/WifiStateMachine.java
+++ wifi/java/android/net/wifi/WifiStateMachine.java
@@ -110,6 +110,7 @@ public class WifiStateMachine extends StateMachine {
     private ConnectivityManager mCm;
 
     private final boolean mP2pSupported;
+    private boolean mIbssSupported;
     private final AtomicBoolean mP2pConnected = new AtomicBoolean(false);
     private boolean mTemporarilyDisconnectWifi = false;
     private final String mPrimaryDeviceType;
@@ -352,7 +353,12 @@ public class WifiStateMachine extends StateMachine {
     public static final int CMD_DISABLE_P2P_REQ           = BASE + 132;
     public static final int CMD_DISABLE_P2P_RSP           = BASE + 133;
 
-    public static final int CMD_BOOT_COMPLETED            = BASE + 134;
+    public static final int CMD_BOOT_COMPLETED            = BASE + 136;
+
+    /* Is IBSS mode supported by the driver? */  
+    public static final int CMD_GET_IBSS_SUPPORTED        = BASE + 134;  
+  
+
 
     public static final int CONNECT_MODE                   = 1;
     public static final int SCAN_ONLY_MODE                 = 2;
@@ -1067,6 +1073,14 @@ public class WifiStateMachine extends StateMachine {
     public String getCountryCode() {
         return mCountryCode;
     }
+
+    public int syncIsIbssSupported(AsyncChannel channel) {  
+        Message resultMsg = channel.sendMessageSynchronously(CMD_GET_IBSS_SUPPORTED);  
+        int result = resultMsg.arg1;  
+        resultMsg.recycle();  
+        return result;  
+    }  
+  
 
     /**
      * Set the operational frequency band
@@ -1947,6 +1961,7 @@ public class WifiStateMachine extends StateMachine {
                 case CMD_ADD_OR_UPDATE_NETWORK:
                 case CMD_REMOVE_NETWORK:
                 case CMD_SAVE_CONFIG:
+                case CMD_GET_IBSS_SUPPORTED:
                     replyToMessage(message, message.what, FAILURE);
                     break;
                 case CMD_GET_CONFIGURED_NETWORKS:
@@ -2214,6 +2229,8 @@ public class WifiStateMachine extends StateMachine {
                     mWifiConfigStore.loadAndEnableAllNetworks();
                     initializeWpsDetails();
 
+                    mIbssSupported = mWifiNative.getModeCapability("IBSS");  
+
                     sendSupplicantConnectionChangedBroadcast(true);
                     transitionTo(mDriverStartedState);
                     break;
@@ -2242,6 +2259,7 @@ public class WifiStateMachine extends StateMachine {
                 case CMD_SET_FREQUENCY_BAND:
                 case CMD_START_PACKET_FILTERING:
                 case CMD_STOP_PACKET_FILTERING:
+                case CMD_GET_IBSS_SUPPORTED: 
                     deferMessage(message);
                     break;
                 default:
@@ -2305,6 +2323,10 @@ public class WifiStateMachine extends StateMachine {
                 case CMD_SET_OPERATIONAL_MODE:
                     mOperationalMode = message.arg1;
                     break;
+                case CMD_GET_IBSS_SUPPORTED:  
+                    deferMessage(message);  
+                    break;  
+
                 default:
                     return NOT_HANDLED;
             }
@@ -2621,6 +2643,9 @@ public class WifiStateMachine extends StateMachine {
                         setSuspendOptimizationsNative(SUSPEND_DUE_TO_HIGH_PERF, true);
                     }
                     break;
+                case CMD_GET_IBSS_SUPPORTED:  
+                    replyToMessage(message, message.what, mIbssSupported ? 1 : 0);  
+                    break;  
                 default:
                     return NOT_HANDLED;
             }
